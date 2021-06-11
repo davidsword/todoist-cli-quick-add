544f3ab13731078a98bd0c6a4b99a5d2ebdff161
David Sword <david@davidsword.ca>
2021-06-11 16:55:01 -0700

    add sanitization todo

--- todoist.js
+++ todoist.js
@@ -12,12 +12,13 @@ const todoist = Todoist(process.env.TODOIST_API)
 
 // get it
 var todoTask = sanitize(process.argv.slice(2).join(' '));
+  // @TODO sanitization too strong, strips out slashes from links
   // @TODO check for empty string
   // @TODO add args for placing directly in projects/tags, etc.
 
 // fire it
 ;(async () => {
   const newItem = await todoist.items.add({ content: todoTask })
-  //@TODO check for failures
+    // @TODO check for failures
   console.log( chalk.green.bold( 'Added Todo: ' + todoTask ) );
 })()
